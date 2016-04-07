ecd951795e98ffed788ee6970665858271550e69
Benjamin Brasseur <benjamin.brasseur85@gmail.com>
2016-04-07 17:27:09 +0200

    nothing

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ io.on('connection', function(socket){
             io.emit('user list',element.user);
         });
     });
-
     socket.on('disconnect', function(){
         console.log('a user disconnected');
         if(socket.user!=null){
